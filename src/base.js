600e6b684a824d40d0a531bff0e7c097a15aec63
serghov <sergey.hovakimyan@gmail.com>
2017-01-11 14:29:51 +0400

    add display to cardboard event

--- src/base.js
+++ src/base.js
@@ -339,7 +339,7 @@ VRDisplay.prototype.getLayers = function() {
 };
 
 VRDisplay.prototype.fireVRDisplayPresentChange_ = function() {
-  var event = new CustomEvent('vrdisplaypresentchange', {detail: {display: this}});
+  var event = new CustomEvent('vrdisplaypresentchange', {detail: {display: this}, display: this});
   window.dispatchEvent(event);
 };
 
